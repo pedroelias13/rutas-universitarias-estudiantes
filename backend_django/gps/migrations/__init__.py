52599b54827f1eee1aefc2476877b39eff5897dc
Pedro <paponte@uniguajira.edu.co>
2025-11-06 12:08:28 -0500

    Remove gps from backend_django; move to backend/gps

--- backend_django/gps/migrations/__init__.py
+++
@@ -1,4 +0,0 @@
-# Este archivo indica que esta carpeta debe ser tratada como un paquete de Python.
-# Las migraciones serán detectadas y aplicadas por Django a través de este paquete.
-
-# Dejar este archivo vacío intencionalmente.
